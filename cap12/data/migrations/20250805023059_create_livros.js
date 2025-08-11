ca91059b4d4aac98519f9dbe8ddcdbf24e275ce0
Pedro Konig Scheurer <pedrokgsr@hotmail.com>
2025-08-10 23:01:42 -0300

    feat(cap13): Ajuste no index, app e inclusao de componentes e config.axios para react e webservice

--- cap12/data/migrations/20250805023059_create_livros.js
+++ cap12/data/migrations/20250805023059_create_livros.js
@@ -1,27 +1,14 @@
-exports.seed = function (knex) {
-  return knex("livros").del()
-    .then(function () {
-      return knex("livros").insert([
-        {
-          titulo: "Web Design Responsivo", autor: "Maurício Samy Silva", ano: 2014,
-          preco: 73.0, foto: "https://s3.novatec.com.br/capas/9788575223925.jpg",
-        },
-        {
-          titulo: "Proteção Moderna de Dados", autor: "W. Curtis Preston", ano: 2021,
-          preco: 97.0, foto: "https://s3.novatec.com.br/capas/9786586057843.jpg",
-        },
-        {
-          titulo: "SQL em 10 Minutos por Dia", autor: "Ben Forta", ano: 2021,
-          preco: 79.0, foto: "https://s3.novatec.com.br/capas/9786586057447.jpg",
-        },
-        {
-          titulo: "CSS Grid Layout", autor: "Maurício Samy Silva", ano: 2017,
-          preco: 45.0, foto: "https://s3.novatec.com.br/capas/9788575226322.jpg",
-        },
-        {
-          titulo: "Python para análise de dados", autor: "Wes McKinney", ano: 2018,
-          preco: 132.0, foto: "https://s3.novatec.com.br/capas/9788575226476.jpg",
-        },
-      ])
+exports.up = function (knex) {
+    return knex.schema.createTable("livros", (table) => {
+        table.increments()
+        table.string("titulo", 80).notNullable()
+        table.string("autor", 60).notNullable()
+        table.integer("ano", 4).notNullable()
+        table.decimal("preco", 9.2).notNullable()
+        table.string("foto", 100).notNullable()
     })
+}
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("livros")
 }
---
+++ cap13/src/components/MenuSuperior.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const MenuSuperior = () => {
+    return(
+        <nav className="navbar navbar-expand-sm bg-primary navbar-dark sticky-top">
+            <div className="container">
+                <Link to="/" className="navbar-brand" href="/">Controle Pessoal de Livros</Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" href="/">Inclusão</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/lista" className="nav-link" href="/">Manutenção</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/controle" className="nav-link" href="/">Resumo</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+export default MenuSuperior;
